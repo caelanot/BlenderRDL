8a1bb6032f2861c21b6ddea5f4c88eaf8f0b437e
Caelan <15092850+caelanot@users.noreply.github.com>
2024-05-22 04:40:56 -0700

    Fixed time starts

--- main.py
+++ main.py
@@ -36,11 +36,16 @@ client = Blender(intent=intents)
 
 @client.event
 async def on_ready():
-    print(f'Logged in as {client.user} (ID: {client.user.id})')
-    print('------')
+    print(f"Logged in as {client.user} (ID: {client.user.id})")
+    print("------")
     client.morgue = await client.fetch_channel(362784581344034816)
     client.pharmacy = await client.fetch_channel(419900766279696384)
-    await client.change_presence(activity=discord.CustomActivity("Making coffee ☕", emoji=discord.PartialEmoji.from_str("coffee")))
+    await client.change_presence(
+        activity=discord.CustomActivity(
+            "Making coffee ☕", emoji=discord.PartialEmoji.from_str("coffee")
+        )
+    )
+    get_blend.start()
 
 
 @client.tree.command(
@@ -52,11 +57,11 @@ async def on_ready():
 async def blend_today(interaction: discord.Interaction, level: str):
     client.blend = level
     await interaction.response.send_message(f"Blending {level} today.")
+    print(f"Blending {level} today.")
 
 
 @client.tree.command(
-    name="randomblend",
-    description="Adds level to list of random blends"
+    name="randomblend", description="Adds level to list of random blends"
 )
 @commands.has_any_role(*roles)
 @app_commands.describe(level="Level to blend")
@@ -64,6 +69,7 @@ async def random_blend(interaction: discord.Interaction, level: str):
     with open("random.txt", "a") as f:
         f.write(f"{level}\n")
     await interaction.response.send_message(f"Blending {level} randomly.")
+    print(f"Blending {level} randomly.")
 
 
 @client.tree.command(
@@ -71,15 +77,13 @@ async def random_blend(interaction: discord.Interaction, level: str):
     description="Schedules level to blend on date",
 )
 @commands.has_any_role(*roles)
-@app_commands.describe(
-    level="level to blend",
-    date="MM DD"
-)
+@app_commands.describe(level="level to blend", date="MM DD")
 async def date_blend(interaction: discord.Interaction, level: str, date: str):
     db = shelve.open("scheduled")
     db[date] = level
     db.close()
     await interaction.response.send_message(f"Blending {level} on {date}.")
+    print(f"Blending {level} on {date}.")
 
 
 @client.tree.command(
@@ -91,12 +95,10 @@ async def date_blend(interaction: discord.Interaction, level: str, date: str):
 async def force_blend(interaction: discord.Interaction, level: str):
     await blend_level(level)
     await interaction.response.send_message("Blending!")
+    print(f"Force blend {level}.")
 
 
-@client.tree.command(
-    name="viewqueue",
-    description="View levels in queue"
-)
+@client.tree.command(name="viewqueue", description="View levels in queue")
 @commands.has_any_role(*roles)
 async def view_queue(interaction: discord.Interaction):
     db = shelve.open("scheduled")
@@ -106,13 +108,10 @@ async def view_queue(interaction: discord.Interaction):
     await interaction.response.send_message(res)
 
 
-@client.tree.command(
-    name="viewrandom",
-    description="View levels in random"
-)
+@client.tree.command(name="viewrandom", description="View levels in random")
 @commands.has_any_role(*roles)
 async def view_random(interaction: discord.Interaction):
-    with open("random", "r") as f:
+    with open("random.txt", "r") as f:
         await interaction.response.send_message(f.read())
 
 
@@ -123,16 +122,16 @@ async def get_blend():
         client.blend = ""
     else:
         try:
-            today = datetime.date.today()
+            today = datetime.datetime.now(datetime.UTC).today()
             today_str = today.strftime("%m %d")
             db = shelve.open("scheduled")
             await blend_level(db[today_str])
             del db[today_str]
         except KeyError:
-            levels = open("random").read().splitlines()
+            levels = open("random.txt").read().splitlines()
             print(levels)
             await blend_level(levels.pop(random.randrange(len(levels))))
-            with open("random", "w") as f:
+            with open("random.txt", "w") as f:
                 for level in levels:
                     f.write(f"{level}\n")
 
@@ -140,5 +139,6 @@ async def get_blend():
 async def blend_level(level: str):
     await client.morgue.send(f"rdzip^blend {level}")
     print(f"Blending {level} at {datetime.datetime.now()}")
+
 
 client.run(token)
